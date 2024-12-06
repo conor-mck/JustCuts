c17ffd72e845f027c468dab216c0e980152e92b3
conor-mck <conor.mckenna182@gmail.com>
2024-12-06 18:58:36 -0500

    Delete JustCuts directory

--- JustCuts/js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Toggle Navbar
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-});
